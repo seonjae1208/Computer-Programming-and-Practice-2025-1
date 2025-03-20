de4c4fd49b181e2b75b854363635dd0dae403adb
chaebkimm <chae.b.kim@gmail.com>
2025-03-20 20:15:38 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,37 @@
-/* 문자를 입력받아서 문자를 출력하기 */
+/* 문자를 입력받고 포맷팅해서 출력하기 */
 /* 
- * 주어진 print_3chars, input_char 함수를 활용해서 
- * 문자 세개를 입력받고, 화면에 입력받은 문자들을 출력해보세요.
+ * 주어진 add_to_format, print_formatted, input_char 함수를 활용해서 
+ * 포맷을 만들고, 포맷에 따라서 화면에 입력받은 문자 하나를 문자 포맷과 숫자 포맷으로 출력해보세요.
  */
 
 /* 
- * print_3chars 함수
+ * add_to_format(char c) 함수
  *
- * 화면에 char 타입의 문자 세개를 표시합니다.
+ * 입력받은 문자를 포맷에 추가합니다.
+ * 현재 포맷에 문자가 10개였으면, 에러 메시지 출력 후 포맷을 초기화합니다.
  * 
  * 매개변수: 
- * a: char 타입. 화면에 표시할 첫번째 문자
- * b: char 타입. 화면에 표시할 두번째 문자
- * c: char 타입. 화면에 표시할 세번째 문자
+ * c: char 타입. 포맷에 추가할 문자
  *
  * 반환값:
  * 없음
  */
-void print_3chars(char a, char b, char c);
+void add_to_format(char c);
+
+/* 
+ * print_formatted(char a, char b) 함수
+ *
+ * 정해진 포맷에 따라 인자 두개를 화면에 출력합니다.
+ * %c 포맷은 출력할 때 문자로 바뀝니다.
+ * %d 포맷은 출력할 때 숫자로 바뀝니다.
+ * 
+ * 매개변수: 
+ * a: char 타입. 출력할 첫번째 문자
+ * b: char 타입. 출력할 두번째 문자
+ * 반환값:
+ * 없음
+ */
+void print_formatted(char a, char b);
 
 /* 
  * input_char 함수
@@ -36,8 +50,7 @@ void print_3chars(char a, char b, char c);
  */
 char input_char();
 
-
 int main() {
-	/* To Do: 주어진 함수들을 활용해서 문자 세개를 입력받고, 화면에 출력해보세요 */
+	/* To Do: 주어진 함수들을 활용해서 문자 하나를 입력받고, 문자 형식과 숫자 형식으로 출력해보세요 */
 	return 0;
 }
